3fa1bb8e2aa791e603077dee7b7913ec0adb7432
Fahyvor <favourokafor30@gmail.com>
2023-09-25 23:44:16 +0100

    Login and Register pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,20 @@ import Homepage from './Homepage/Homepage';
 import Products from './ProductsPage/Products';
 import SellWaste from './SellersPage/SellWaste';
 import Dashboard from './Dashboard/Dashboard';
+import Login from './Login/Login';
+import Register from './Register/Register';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Homepage />} />
+        <Route path='/home' element={<Homepage />} />
         <Route path='/buy' element={<Products />} />
         <Route path='/sell' element={<SellWaste />} />
         <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/' element={<Login />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </Router>
   );
